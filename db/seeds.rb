32d4855425ec7fc00f6d6f95c661c24ea2fe2243
dustyRose12 <dustyrose125@gmail.com>
2017-08-28 21:28:25 -0500

    contacts-app 1st day 8.28

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+contact = Contact.new(
+                                      first_name: "Alice",
+                                      last_name: "Adams",
+                                      email: "alice.adams@gmail.com",
+                                      phone_number: "123-456-7890"
+                                      )
+
+contact.save
+
+contact = Contact.new(
+                                      first_name: "Beatrix",
+                                      last_name: "Barrow",
+                                      email: "beatrix.barrow@gmail.com",
+                                      phone_number: "234-567-8901"
+                                      )
+
+contact.save
+
+contact = Contact.new(
+                                      first_name: "Cyrus",
+                                      last_name: "Cifrian",
+                                      email: "cyrus.cifrian@gmail.com",
+                                      phone_number: "345-678-9012"
+                                      )
+
+contact.save
+
